19c43f20ff7726dc4ce7741c1b43cda1117d6c4d
waynnbz <waylonago@gmail.com>
2022-06-08 09:42:07 +0800

    init CRA

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -1,34 +1,99 @@
+const asyncHandler = require("express-async-handler");
+
+const Goal = require("../models/goalModel");
+const User = require("../models/userModel");
+
 // @desc    Get goals
 // @route   GET /api/goals
 // @access  Private
-const getGoals = (req, res) => {
-    res.status(200).json({ message: 'Get goals' })
-}
+const getGoals = asyncHandler(async (req, res) => {
+  const goals = await Goal.find({ user: req.user._id });
+
+  res.status(200).json(goals);
+});
 
 // @desc    Set goal
 // @route   POST /api/goals
 // @access  Private
-const setGoal = (req, res) => {
-    res.status(200).json({ message: 'Set goals' })
-}
+const setGoal = asyncHandler(async (req, res) => {
+  if (!req.body.text) {
+    throw new Error("Please add a text field");
+  }
+
+  const goal = await Goal.create({
+    text: req.body.text,
+    user: req.user.id,
+  });
+
+  res.status(200).json(goal);
+});
 
 // @desc    Update goal
 // @route   PUT /api/goals/:id
 // @access  Private
-const updateGoal = (req, res) => {
-    res.status(200).json({ message: `Update goal ${req.params.id}` })
-}
+const updateGoal = asyncHandler(async (req, res) => {
+  const goal = await Goal.findById(req.params.id);
+
+  // Check for goal
+  if (!goal) {
+    res.status(404);
+    throw new Error("Goal not found");
+  }
+
+  const user = await User.findById(req.user.id);
+
+  // Check for user
+  if(!user) {
+    res.status(404);
+    throw new Error("User not found");
+  }
+
+  // Make sure the logged in user matches the goal user
+  if(goal.user.toString() !== user.id) {
+    res.status(401);
+    throw new Error("User not authorized");
+  }
+
+  const updatedGoal = await Goal.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+  });
+
+  res.status(200).json(updatedGoal);
+});
 
 // @desc    Delete goal
 // @route   DELETE /api/goals/:id
 // @access  Private
-const deleteGoal = (req, res) => {
-    res.status(200).json({ message: `Delete goals ${req.params.id}` })
-}
+const deleteGoal = asyncHandler(async (req, res) => {
+  const goal = await Goal.findById(req.params.id);
+
+  if (!goal) {
+    res.status(404);
+    throw new Error("Goal not found");
+  }
+
+  const user = await User.findById(req.user.id);
+
+  // Check for user
+  if(!user) {
+    res.status(404);
+    throw new Error("User not found");
+  }
+
+  // Make sure the logged in user matches the goal user
+  if(goal.user.toString() !== user.id) {
+    res.status(401);
+    throw new Error("User not authorized");
+  }
+
+  await goal.remove();
+
+  res.status(200).json({ id: req.params.id });
+});
 
 module.exports = {
-    getGoals,
-    setGoal,
-    updateGoal,
-    deleteGoal
-}
+  getGoals,
+  setGoal,
+  updateGoal,
+  deleteGoal,
+};
--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,20 @@
-const express = require('express')
-const dotenv = require('dotenv').config()
-const port = process.env.PORT || 5000
+const express = require("express");
+const colors = require("colors");
+const dotenv = require("dotenv").config();
+const { errorHandler } = require("./middleware/errorMiddleware");
+const connectDB = require("./config/db");
+const port = process.env.PORT || 5000;
 
-const app = express()
+connectDB();
 
-app.use('/api/goals', require('./routes/goalRoutes'))
+const app = express();
 
-app.listen(port, () => console.log(`Server started on port ${port}`))
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.use("/api/goals", require("./routes/goalRoutes"));
+app.use("/api/users", require("./routes/userRoutes"));
+
+app.use(errorHandler);
+
+app.listen(port, () => console.log(`Server started on port ${port}`));
